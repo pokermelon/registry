f3b44d83f9f5559f4e11b2a3c22913a60bbfc2f3
Sorawit Suriyakarn <thepsint@gmail.com>
2025-06-15 11:51:56 +0700

    Add full example

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import yamale
 
 
+def validate(schema_path: str, data_path: str):
+    schema = yamale.make_schema(schema_path)
+    data = yamale.make_data(data_path)
+    yamale.validate(schema, data)
+
+
 def main():
-    schema = yamale.make_schema("./schema/tournament.yaml")
-    data = yamale.make_data("./data/tournaments/appt-manila-2025.yaml")
-    print(yamale.validate(schema, data))
+    validate("./schema/festival.yaml", "./data/festivals/appt-manila-2025.yaml")
+    validate("./schema/series.yaml", "./data/series/appt.yaml")
+    validate("./schema/venue.yaml", "./data/venues/ph-okada-manila.yaml")
 
 
 if __name__ == "__main__":
